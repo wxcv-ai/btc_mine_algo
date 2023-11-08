8831e851bc1152f3b2134dda80683c916ee77884
Achref Soltani <78027438+wxcv-ai@users.noreply.github.com>
2023-11-08 22:48:00 +0100

    Add files via upload

---
+++ CryptographyBTCGen-main/cryptos/curves.py
@@ -0,0 +1,125 @@
+"""
+Core functions for math over Elliptic Curves over Finite Fields,
+especially the ability to define Points on Curves and perform
+addition and scalar multiplication.
+"""
+from __future__ import annotations # PEP 563: Postponed Evaluation of Annotations
+from dataclasses import dataclass
+import json
+
+# -----------------------------------------------------------------------------
+# public API
+
+__all__ = ['Curve', 'Point', 'Generator']
+
+# -----------------------------------------------------------------------------
+# Related math utilities
+
+def extended_euclidean_algorithm(a, b):
+    """
+    Returns (gcd, x, y) s.t. a * x + b * y == gcd
+    This function implements the extended Euclidean
+    algorithm and runs in O(log b) in the worst case,
+    taken from Wikipedia.
+    """
+    old_r, r = a, b
+    old_s, s = 1, 0
+    old_t, t = 0, 1
+    while r != 0:
+        quotient = old_r // r
+        old_r, r = r, old_r - quotient * r
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+    return old_r, old_s, old_t
+
+def inv(n, p):
+    """ returns modular multiplicate inverse m s.t. (n * m) % p == 1 """
+    gcd, x, y = extended_euclidean_algorithm(n, p) # pylint: disable=unused-variable
+    return x % p
+
+# -----------------------------------------------------------------------------
+# Core data structures to represent curves and generators
+
+@dataclass
+class Curve:
+    """
+    Elliptic Curve over the field of integers modulo a prime.
+    Points on the curve satisfy y^2 = x^3 + a*x + b (mod p).
+    """
+    p: int
+    a: int
+    b: int
+
+@dataclass
+class Point:
+    """ An integer point (x,y) on a Curve """
+    curve: Curve
+    x: int
+    y: int
+    all_data_test = []
+    def __add__(self, other: Point) -> Point:
+        # handle special case of P + 0 = 0 + P = 0
+        print("begin")
+        value = ""
+        if self == INF:
+            return other
+        if other == INF:
+            return self
+        # handle special case of P + (-P) = 0
+        if self.x == other.x and self.y != other.y:
+            return INF
+        # compute the "slope"
+        if self.x == other.x: # (self.y = other.y is guaranteed too per above check)
+            m = (3 * self.x**2 + self.curve.a) * inv(2 * self.y, self.curve.p)
+            value = ("jj")
+        else:
+            m = (self.y - other.y) * inv(self.x - other.x, self.curve.p)
+            value = ("kk")
+        # compute the new point
+        rx = (m**2 - self.x - other.x) % self.curve.p
+        ry = (-(m*(rx - self.x) + self.y)) % self.curve.p
+
+        
+        print("end"  ,'||||',len(str( (m*(rx - self.x) + self.y)  )),"||||",len(str(m)) )
+        print(value)
+        print("\n")
+        Point.all_data_test.append({"value":value,"rx" :rx , "ry" :ry , "m":m , "self_x":self.x ,
+         "self_y":self.y ,"other_x":other.x ,"other_y":other.y 
+         ,"rx_before_mod" :(m**2 - self.x - other.x)
+         ,"ry_before_mod":(-(m*(rx - self.x) + self.y)),"p" :self.curve.p})
+
+         
+        return Point(self.curve, rx, ry)
+
+    def __rmul__(self, k: int) -> Point:
+        assert isinstance(k, int) and k >= 0
+        result = INF
+        append = self
+        #print(len(str(k))*8 , type(result) , type(append))
+        prev_k = k
+        while k:
+            #print("append" ,append)
+            #print("\n")
+            #print("result" ,result)
+            #print("\n")
+            
+            if k & 1:
+                result += append
+                print("result")
+            append += append
+            k >>= 1
+            # break
+
+        file = open("data_test.json","w")
+        file.write((str({"the_data" :Point.all_data_test ,"privkey":prev_k , "pubkey":""}).replace("'",'"')  ))
+        return result
+
+@dataclass
+class Generator:
+    """
+    A generator over a curve: an initial point and the (pre-computed) order
+    """
+    G: Point     # a generator point on the curve
+    n: int       # the order of the generating point, so 0*G = n*G = INF
+
+INF = Point(None, None, None)
